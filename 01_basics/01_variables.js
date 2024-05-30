7949ac276762d3462a9f22d6851476c4c6574815
shivam-kumar-26 <71949630+shivam-kumar-26@users.noreply.github.com>
2024-05-30 18:52:20 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144589
+let accountEmail= "shivam@gmail.com"
+var accountPassword="5555"
+accountCity="Pune"
+let accountState;
+
+// accountId=8585 // const  can't be change or not allowed
+accountEmail="sonu@gmail.com"
+accountPassword="50050"
+accountCity="Gaya"
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity , accountState]);
+
+/* Prefer not to use var(variable) 
+ because of issue in block scope and function scope
+ */
---
+++ 01_basics/02-dataTypes.js
@@ -0,0 +1,57 @@
+"use strict"; // treat all Js code as newer version
+
+// alert (3 + 3 ) // we are using node js  not Browser
+
+console.log(3
+    + 
+    3) // dont use this structure // code readability should be high
+console.log("Shivam")
+
+// documentation of any defined name -> data types or let alert const etc // msotly used to understand -> mdn //
+// actual documentation is -> tc39.es => ecma script // about standard (like si unit of this program)
+
+
+// Types of Data types
+let name ="Shivam"
+let age =22
+let isLoggedIn = true
+
+let temp;
+let Temp = null
+console.table([name,age,isLoggedIn,temp,Temp]);
+
+//******* Primitive data Types ********* */
+// number => Rnage -> 2 ^ 52 -> we store no. upto
+// bigInt => used in stocks to hold big number
+//string => " " or ' ' both we can use
+// boolean => true / false
+// null => stand alone value  
+//          kisi data ya data type me value to h but value hi null h  
+//          (representation of empty value) 
+// undefined =>value missing ya value undefined h 
+//              means variable declare h but value undefined h
+//symbol => used to find uniqueness (uniqe)
+
+
+//object
+
+console.log(typeof null); // special case -> object
+console.log(typeof "Shivam"); // string
+console.log(typeof age); // number
+console.log(typeof name) // string
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof undefined); // undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+

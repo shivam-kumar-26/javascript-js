4bf4e6975a009f6efdac00acee20089db6e720e1
shivam-kumar-26 <71949630+shivam-kumar-26@users.noreply.github.com>
2024-05-31 19:46:40 +0000

    datatypes-summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,14 @@
+//dataTypes -> 2 types -> 1)Primitive  2) Non Primitive(Refference data types)
+//kis trike se data ko memory me rkha jata h ar access kiya jata h us basis pe two data types hain primitive and non primitive
+
+/* ****Primitive******* -> it is CALL BY VALUE datatypes  means jb v aap inko khi se khi copy krte hain
+for use to inka jo original data h memory ka wo nhi diya jata wo us memoey se copy krke diya jata h ar 
+usme hm jo v changes krte hain wo copy me changes hote hain 
+
+// 7 types -> String , Number , Boolean , null(empty) , undefined , Symbol , BigInt
+
+
+ ******Reference (Non primitive)-> ye sari values wo hain jinka reference memory me allocate kiya jaskta h
+//types->  Array , Objects , Functions
+*/
+

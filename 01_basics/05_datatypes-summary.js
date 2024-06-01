d4bc84a92e5795dbb30dac2b629794659ea62aae
shivam-kumar-26 <71949630+shivam-kumar-26@users.noreply.github.com>
2024-06-01 12:17:53 +0000

    dataType-summary

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -6,9 +6,68 @@ for use to inka jo original data h memory ka wo nhi diya jata wo us memoey se co
 usme hm jo v changes krte hain wo copy me changes hote hain 
 
 // 7 types -> String , Number , Boolean , null(empty) , undefined , Symbol , BigInt
+*/
+const score = 100          // number
+const scoreValue = 100.3   //number
+
+const isLoggedIn = false   //boolean
+const outSideTemp= null  // null
+let userEmail; //undefined
+
+//symbol -> used for define uniqueness***************************
+const id = Symbol('555')
+const anotherId = Symbol('555') 
+console.log(id===anotherId); // false // becz of symbol -> they make them unique
+console.log(typeof id);   //symbol
+
+const Id = ('555')
+const anotherIdIs = ('555') 
+console.log(Id===anotherIdIs); // true // becz no use of symbol
+console.log();
+
+//BigInt --> 129552233144n *************************
+const bigNumber= 454545454545n
+console.log(typeof bigNumber);  // bigint
 
 
- ******Reference (Non primitive)-> ye sari values wo hain jinka reference memory me allocate kiya jaskta h
+
+ /*******Reference (Non primitive)-> ye sari values wo hain jinka reference memory me allocate kiya jaskta h
 //types->  Array , Objects , Functions
 */
+
+//Array ********************
+const heros = ["BlackPanther" , "IronMan" , "AntMan"] 
+
+//Object ******************* (curlyBraces{} ke andr jo v hain sb object hain 
+                           //  -> jiske andr dataTypes kuch v hoskta  => String , Number , Boolean , ek dusra Object , function , array etc
+
+//NOTE -> Array , Object , Function ka dataTypes -> Function hi hota h 
+
+let myObject = {
+    name:"Shivam",
+    age: 22,
+    emailId: "shivam@gmail.com",
+    heros : ["IronMan" , "CaptianAmerica"],
+}
+console.log(myObject);
+
+
+//Function  -> function(){}
+
+const myFunction = function(){
+    console.log("Hellow world")
+}
+console.log(myFunction) // [Function: myFunction]  //define**
+
+//some more dataTypes
+console.log(typeof myObject);  //object
+console.log(typeof myFunction); // function (also knwn object function  or function object)
+
+
+
+
+
+
+
+
 

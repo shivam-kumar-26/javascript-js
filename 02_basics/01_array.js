0ee7dcc4df27dd62a2536463c0d52dcaa4d5112c
shivam-kumar-26 <71949630+shivam-kumar-26@users.noreply.github.com>
2024-06-04 07:21:11 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,13 @@
+// Array
+const myArray=[0,1,2,3,4,5]
+console.log(myArray);   //[ 0, 1, 2, 3, 4, 5 ]
+
+//in arr - can contain - number , strings, boolean , arr, object also function
+const myArr = [0 , 1 , 2 , 3 , 4 , 5 , "Shivam" , true , ["Sonu", true], {age:23, emailId: "shivam@gmail.com"}]
+console.log(myArr); //  
+/*
+[ 0,1,2,3,4,5,
+    'Shivam',true,
+    [ 'Sonu', true ],
+    { age: 23, emailId: 'shivam@gmail.com' }
+] */

2567556ef9c1698a3ebda930ce7d7d53b1303f36
shivam-kumar-26 <71949630+shivam-kumar-26@users.noreply.github.com>
2024-06-06 13:41:19 +0000

    ArrowFunction04

--- 03_basic/04_Arraow.js
+++ 03_basic/04_Arraow.js
@@ -23,11 +23,11 @@ const user = {
     }
 }
 // run as object 
-user.welComMessage // run to hoga bt nothing print becz welcomemsg ek method h so we need ( parenthesis)
-user.welComMessage()  //Shivam , welcome to website
+//user.welComMessage // run to hoga bt nothing print becz welcomemsg ek method h so we need ( parenthesis)
+//user.welComMessage()  //Shivam , welcome to website
 //now change user name - means contest change
-user.userName= "sam"
-user.welComMessage()  //sam , welcome to website
+//user.userName= "sam"
+//user.welComMessage()  //sam , welcome to website
 
 
 
@@ -38,13 +38,68 @@ const user1 = {
 
     welComMessages: function() {
         //so use of --this inside the current contenst 
-        console.log(`${this.userName1} , welcome to website`);
+        console.log(`${this.userName1} , welcome to website`); // iske 1st jgh rhne ke karn 1st print 
         console.log(this); // this kya krta h pta chlega
+        //2nd print - becz of console (this)
+        /*{
+           userName1: 'Shiva',
+           prices: 1111,
+           welComMessages: [Function: welComMessages]
+           }*/
+          /* again after 3rd print - 4rth print - console (this)
+           {
+              userName1: 'sam',
+              prices: 1111,
+              welComMessages: [Function: welComMessages]
+            } */
+
     }
 }
-// user.welComMessage // run to hoga bt nothing print becz welcomemsg ek method h so we need ( parenthesis)
-user1.welComMessage()  //Shivam , welcome to website
+user1.welComMessages()  // 1st print - Shiva , welcome to website
+
 //now change user name - means contest change
 user1.userName1= "sam"
-user1.welComMessage()  //sam , welcome to website
+user1.welComMessages()  // 3rd print -  sam , welcome to website
 
+//note this - jo cureent contest ke bare me btata h  - means jo v changement horha h usko v btata h 
+
+// agr - console(this) ko last me print krate hain the what will happen
+const user2 = {
+    userName2:"Shiva",
+    priceList:1111,
+
+    welComMessages: function() {
+        //so use of --this inside the current contenst 
+        console.log(`${this.userName2} , welcome to website`);
+    }
+}
+console.log(this);  /* {}  - - empty object we get - becz hm - nord environment ke andr hain so (this)- reffer to {} empty obj
+                           kynki av global ke andr koi contest hi nhi h isiliye empty. 
+                           phle - js ko execute krne ka ek hi trika tha jo tha browser ke andr jo browser ka engine h jo js ko execute keta tha  wo simply engine ek hi jghy p-aya jata tha jo tha browser ke andr
+                        ab hmne engine ko nikala ar usko alg alg nam diya like nord , dino, bun so ab engine hmare pas stand alone h 
+                        so browser me jb v run krta h b(this) - to global ke andr jo object h - windows object
+  INTERVIEW QUE   ----   so in inspect - console this - global obj - we get windows*/
+
+  //Arrow function
+// note-- Arraow function ke andr this nhi hota h how let see
+function chai_Pani() {
+    let username = "Shivam"
+    console.log(this.username); //undefined- so we can say- Arraow function ke andr this use  nhi hota h
+}
+chai_Pani()
+//simply this is only used in object not in function
+
+//CONFUSION - this ko lekr
+function Pizza_Code() {
+    username = "Shivam"
+    console.log(this.username); // Shivam - here we geeting the value 
+}
+Pizza_Code()
+
+// we find that console(this) - in object - {} - give empty object
+// we find that cosole (this) - in function - in function it is giving Object[global] , clearImmediate ,  setImmediate,  clearInterval, clearTimeout, setTimeout,navigator,performance, crypto etc
+function chai() {
+    console.log(this); // we find here that - in function it is giving Object[global] , clearImmediate ,  setImmediate,  clearInterval, clearTimeout, setTimeout,navigator,performance, crypto etc
+}
+//call function
+chai()

8b32eedd57299295f8a1037af1fa7ebfcfad1950
shivam-kumar-26 <71949630+shivam-kumar-26@users.noreply.github.com>
2024-06-08 10:50:14 +0000

    logical_operator

---
+++ 04_control_flow(logic_flow)/04_logical_operator.js
@@ -0,0 +1,12 @@
+//logical operator
+
+/* a - true , b true , c - false
+&& -- and operator  
+   -- a && b  - true 
+   --a && c - false 
+|| -- or operator 
+   --a || b - true 
+   --a || c - true 
+
+//Nullish Coalescing Operator (??) - we see two keyword -  null undefined */
+
